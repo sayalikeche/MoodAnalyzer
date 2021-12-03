4256b41e70b00a4a0fed7c93fc823f8007622a1f
sayalikeche <92769579+sayalikeche@users.noreply.github.com>
2021-12-03 22:50:26 +0530

    Update Program.cs

--- MoodAnalyzer/MoodAnalyzer/Program.cs
+++ MoodAnalyzer/MoodAnalyzer/Program.cs
@@ -2,15 +2,40 @@
 
 using System;
 
-namespace MoodAnalyzer
+namespace GarbageCollection
 {
-    
-    public class Program
+    class Program
     {
-        
-        public  static void Main(string[]args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Mood Analyzer");
+            long mem1 = GC.GetTotalMemory(false);
+            {
+                int[] values = new int[5000];
+                values = null;
+            }
+            long mem2 = GC.GetTotalMemory(false);
+            {
+                GC.Collect();
+            }
+            long mem3 = GC.GetTotalMemory(false);
+            {
+                Console.WriteLine(mem1);
+                Console.WriteLine(mem2);
+                Console.WriteLine(mem3);
+            }
+            Console.WriteLine("#####################");
+
+            long bytes1 = GC.GetTotalMemory(false);
+            byte[] memory = new byte[1000 * 1000 * 10];
+            memory[0] = 1;
+            long bytes2 = GC.GetTotalMemory(false);
+            long bytes3 = GC.GetTotalMemory(true);
+            Console.WriteLine(bytes1);
+            Console.WriteLine(bytes2);
+            Console.WriteLine(bytes2 - bytes1);
+            Console.WriteLine(bytes3);
+            Console.WriteLine(bytes3 - bytes2);
+            Console.WriteLine();
         }
     }
 }
