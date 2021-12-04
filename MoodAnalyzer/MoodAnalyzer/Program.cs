c9ed68e4d25798bb1547af160a18de3cb6360ced
sayalikeche <92769579+sayalikeche@users.noreply.github.com>
2021-12-04 07:02:36 +0530

    Update Program.cs

--- MoodAnalyzer/MoodAnalyzer/Program.cs
+++ MoodAnalyzer/MoodAnalyzer/Program.cs
@@ -1,41 +1,52 @@
+using MoodAnalyserProblem;
+using NUnit.Framework;
 
-
-using System;
-
-namespace GarbageCollection
+namespace MoodAnalyserTest
 {
-    class Program
+    public class Tests
     {
-        static void Main(string[] args)
+        [SetUp]
+        public void Setup()
         {
-            long mem1 = GC.GetTotalMemory(false);
-            {
-                int[] values = new int[5000];
-                values = null;
-            }
-            long mem2 = GC.GetTotalMemory(false);
-            {
-                GC.Collect();
-            }
-            long mem3 = GC.GetTotalMemory(false);
-            {
-                Console.WriteLine(mem1);
-                Console.WriteLine(mem2);
-                Console.WriteLine(mem3);
-            }
-            Console.WriteLine("#####################");
+        }
 
-            long bytes1 = GC.GetTotalMemory(false);
-            byte[] memory = new byte[1000 * 1000 * 10];
-            memory[0] = 1;
-            long bytes2 = GC.GetTotalMemory(false);
-            long bytes3 = GC.GetTotalMemory(true);
-            Console.WriteLine(bytes1);
-            Console.WriteLine(bytes2);
-            Console.WriteLine(bytes2 - bytes1);
-            Console.WriteLine(bytes3);
-            Console.WriteLine(bytes3 - bytes2);
-            Console.WriteLine();
+        [Test]
+        public void GivenSadMessage_WhenAnalyze_ShouldReturnSad()
+        {
+            //Arrange
+            string message = "Iam in Sad Mood";
+            string expected = "Sad";
+            //Act
+            MoodAnalyser mood = new MoodAnalyser(message);
+            string result = mood.AnalyserMood();
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void WhenGivenHappyMood_ReturnsHappyMessage()
+        {
+
+            //Arrange
+            string message = "Iam in Any Mood";
+            string expected = "Happy";
+            //Act
+            MoodAnalyser mood = new MoodAnalyser(message);
+            string result = mood.AnalyserMood();
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void WhenGivenNullMood_ReturnsHappyMessage()
+        {
+
+            //Arrange
+            string message = "null";
+            string expected = "Happy";
+            //Act
+            MoodAnalyser mood = new MoodAnalyser(message);
+            string result = mood.AnalyserMood();
+            //Assert
+            Assert.AreEqual(expected, result);
         }
     }
 }
